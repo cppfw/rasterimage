1e7abb27d0ddda5eec43d712f436585c2366ff4e
Ivan Gagis <igagis@gmail.com>
2023-05-25 19:37:18 +0300

    stuff

--- src/rasterimage/image.hpp
+++ src/rasterimage/image.hpp
@@ -2,121 +2,102 @@
 
 #include <variant>
 
-#include "channel.hpp"
 #include "static_format_image.hpp"
 
 namespace rasterimage {
 
 enum class depth {
-	uint_8_bit,
-	uint_16_bit,
-	floating_point,
+  uint_8_bit,
+  uint_16_bit,
+  floating_point,
 
-	enum_size
+  enum_size
 };
 
 template <depth depth_enum>
 using depth_type_t = std::conditional_t<
-	depth_enum == depth::uint_8_bit,
-	uint8_t,
-	std::conditional_t<depth_enum == depth::uint_16_bit, uint16_t, float>>;
+    depth_enum == depth::uint_8_bit, uint8_t,
+    std::conditional_t<depth_enum == depth::uint_16_bit, uint16_t, float>>;
 
 enum class components {
-	grey,
-	greya,
-	rgb,
-	rgba,
+  grey,
+  greya,
+  rgb,
+  rgba,
 
-	enum_size
+  enum_size
 };
 
-inline constexpr size_t to_num_channels(components f)
-{
-	return size_t(f) + 1;
-}
+inline constexpr size_t to_num_channels(components f) { return size_t(f) + 1; }
 
 // TODO: doxygen
-class image
-{
+class image {
 public:
-	using variant_type = std::variant<
-		static_format_image<channel<uint8_t>, 1>,
-		static_format_image<channel<uint8_t>, 2>,
-		static_format_image<channel<uint8_t>, 3>,
-		static_format_image<channel<uint8_t>, 4>,
-		static_format_image<channel<uint16_t>, 1>,
-		static_format_image<channel<uint16_t>, 2>,
-		static_format_image<channel<uint16_t>, 3>,
-		static_format_image<channel<uint16_t>, 4>,
-		static_format_image<channel<float>, 1>,
-		static_format_image<channel<float>, 2>,
-		static_format_image<channel<float>, 3>,
-		static_format_image<channel<float>, 4>>;
+  using variant_type = std::variant<    //
+      static_format_image<uint8_t, 1>,  //
+      static_format_image<uint8_t, 2>,  //
+      static_format_image<uint8_t, 3>,  //
+      static_format_image<uint8_t, 4>,  //
+      static_format_image<uint16_t, 1>, //
+      static_format_image<uint16_t, 2>, //
+      static_format_image<uint16_t, 3>, //
+      static_format_image<uint16_t, 4>, //
+      static_format_image<float, 1>,    //
+      static_format_image<float, 2>,    //
+      static_format_image<float, 3>,    //
+      static_format_image<float, 4>     //
+      >;
 
 private:
-	variant_type variant;
+  variant_type variant;
 
-	static size_t to_variant_index(components pixel_components, depth channel_depth);
+  static size_t to_variant_index(components pixel_components,
+                                 depth channel_depth);
 
 public:
-	image(
-		const r4::vector2<uint32_t>& dimensions = {0, 0},
-		components pixel_components = components::rgba,
-		depth channel_depth = depth::uint_8_bit
-	);
+  image(const r4::vector2<uint32_t> &dimensions = {0, 0},
+        components pixel_components = components::rgba,
+        depth channel_depth = depth::uint_8_bit);
 
-	size_t num_channels() const noexcept
-	{
-		auto ret = size_t(this->get_components()) + 1;
-		ASSERT(
-			std::visit(
-				[](const auto& sfi) {
-					return sfi.num_channels;
-				},
-				this->variant
-			)
-			== ret
-		)
-		return ret;
-	}
+  size_t num_channels() const noexcept {
+    auto ret = size_t(this->get_components()) + 1;
+    ASSERT(std::visit([](const auto &sfi) { return sfi.num_channels; },
+                      this->variant) == ret)
+    return ret;
+  }
 
-	components get_components() const noexcept
-	{
-		auto ret = components(this->variant.index() % size_t(components::enum_size));
-		ASSERT(
-			components(std::visit(
-				[](const auto& sfi) {
-					return sfi.num_channels - 1;
-				},
-				this->variant
-			))
-			== ret
-		)
-		return ret;
-	}
+  components get_components() const noexcept {
+    auto ret =
+        components(this->variant.index() % size_t(components::enum_size));
+    ASSERT(components(
+               std::visit([](const auto &sfi) { return sfi.num_channels - 1; },
+                          this->variant)) == ret)
+    return ret;
+  }
 
-	depth get_depth() const noexcept
-	{
-		return depth(this->variant.index() / size_t(components::enum_size));
-	}
+  depth get_depth() const noexcept {
+    return depth(this->variant.index() / size_t(components::enum_size));
+  }
 
-	const dimensioned::dimensions_type& dims() const noexcept;
+  const dimensioned::dimensions_type &dims() const noexcept;
 
-	template <components components_enum, depth depth_enum = depth::uint_8_bit>
-	static_format_image<channel<depth_type_t<depth_enum>>, to_num_channels(components_enum)>& get()
-	{
-		return std::get<static_format_image<channel<depth_type_t<depth_enum>>, to_num_channels(components_enum)>>(
-			this->variant
-		);
-	}
+  template <components components_enum, depth depth_enum = depth::uint_8_bit>
+  static_format_image<depth_type_t<depth_enum>,
+                      to_num_channels(components_enum)> &
+  get() {
+    return std::get<static_format_image<depth_type_t<depth_enum>,
+                                        to_num_channels(components_enum)>>(
+        this->variant);
+  }
 
-	template <components components_enum, depth depth_enum = depth::uint_8_bit>
-	const static_format_image<channel<depth_type_t<depth_enum>>, to_num_channels(components_enum)>& get() const
-	{
-		return std::get<static_format_image<channel<depth_type_t<depth_enum>>, to_num_channels(components_enum)>>(
-			this->variant
-		);
-	}
+  template <components components_enum, depth depth_enum = depth::uint_8_bit>
+  const static_format_image<depth_type_t<depth_enum>,
+                            to_num_channels(components_enum)> &
+  get() const {
+    return std::get<static_format_image<depth_type_t<depth_enum>,
+                                        to_num_channels(components_enum)>>(
+        this->variant);
+  }
 };
 
 } // namespace rasterimage
--- tests/unit/src/image.cpp
+++ tests/unit/src/image.cpp
@@ -3,56 +3,58 @@
 #include <tst/set.hpp>
 
 namespace {
-tst::set set("image", [](tst::suite& suite) {
-	suite.add<std::tuple<rasterimage::components, rasterimage::depth, size_t>>(
-		"image_num_channels",
-		{
-			{ rasterimage::components::grey,     rasterimage::depth::uint_8_bit, 1},
-			{rasterimage::components::greya,     rasterimage::depth::uint_8_bit, 2},
-			{  rasterimage::components::rgb,     rasterimage::depth::uint_8_bit, 3},
-			{ rasterimage::components::rgba,     rasterimage::depth::uint_8_bit, 4},
-			{ rasterimage::components::grey,    rasterimage::depth::uint_16_bit, 1},
-			{rasterimage::components::greya,    rasterimage::depth::uint_16_bit, 2},
-			{  rasterimage::components::rgb,    rasterimage::depth::uint_16_bit, 3},
-			{ rasterimage::components::rgba,    rasterimage::depth::uint_16_bit, 4},
-			{ rasterimage::components::grey, rasterimage::depth::floating_point, 1},
-			{rasterimage::components::greya, rasterimage::depth::floating_point, 2},
-			{  rasterimage::components::rgb, rasterimage::depth::floating_point, 3},
-			{ rasterimage::components::rgba, rasterimage::depth::floating_point, 4},
-    },
-		[](const auto& p) {
-			rasterimage::image im{
-				{10, 20},
-				std::get<rasterimage::components>(p),
-				std::get<rasterimage::depth>(p)
-            };
+tst::set set("image", [](tst::suite &suite) {
+  suite.add<std::tuple<rasterimage::components, rasterimage::depth, size_t>>(
+      "image_num_channels",
+      {
+          {rasterimage::components::grey, rasterimage::depth::uint_8_bit, 1},
+          {rasterimage::components::greya, rasterimage::depth::uint_8_bit, 2},
+          {rasterimage::components::rgb, rasterimage::depth::uint_8_bit, 3},
+          {rasterimage::components::rgba, rasterimage::depth::uint_8_bit, 4},
+          {rasterimage::components::grey, rasterimage::depth::uint_16_bit, 1},
+          {rasterimage::components::greya, rasterimage::depth::uint_16_bit, 2},
+          {rasterimage::components::rgb, rasterimage::depth::uint_16_bit, 3},
+          {rasterimage::components::rgba, rasterimage::depth::uint_16_bit, 4},
+          {rasterimage::components::grey, rasterimage::depth::floating_point,
+           1},
+          {rasterimage::components::greya, rasterimage::depth::floating_point,
+           2},
+          {rasterimage::components::rgb, rasterimage::depth::floating_point, 3},
+          {rasterimage::components::rgba, rasterimage::depth::floating_point,
+           4},
+      },
+      [](const auto &p) {
+        rasterimage::image im{{10, 20},
+                              std::get<rasterimage::components>(p),
+                              std::get<rasterimage::depth>(p)};
 
-			tst::check_eq(im.num_channels(), std::get<size_t>(p), SL);
-			tst::check(im.get_components() == std::get<rasterimage::components>(p), SL);
-			tst::check(im.get_depth() == std::get<rasterimage::depth>(p), SL);
+        tst::check_eq(im.num_channels(), std::get<size_t>(p), SL);
+        tst::check(im.get_components() == std::get<rasterimage::components>(p),
+                   SL);
+        tst::check(im.get_depth() == std::get<rasterimage::depth>(p), SL);
 
-			rasterimage::image empty_im{
-				{0, 0},
-				std::get<rasterimage::components>(p),
-				std::get<rasterimage::depth>(p)
-            };
+        rasterimage::image empty_im{{0, 0},
+                                    std::get<rasterimage::components>(p),
+                                    std::get<rasterimage::depth>(p)};
 
-			tst::check_eq(empty_im.num_channels(), std::get<size_t>(p), SL);
-			tst::check(empty_im.get_components() == std::get<rasterimage::components>(p), SL);
-			tst::check(empty_im.get_depth() == std::get<rasterimage::depth>(p), SL);
-		}
-	);
+        tst::check_eq(empty_im.num_channels(), std::get<size_t>(p), SL);
+        tst::check(empty_im.get_components() ==
+                       std::get<rasterimage::components>(p),
+                   SL);
+        tst::check(empty_im.get_depth() == std::get<rasterimage::depth>(p), SL);
+      });
 
-	suite.add("image_dims", []() {
-		rasterimage::dimensioned::dimensions_type expected = {13, 20};
+  suite.add("image_dims", []() {
+    rasterimage::dimensioned::dimensions_type expected = {13, 20};
 
-		for (auto d = 0; d != size_t(rasterimage::depth::enum_size); ++d) {
-			for (auto f = 0; f != size_t(rasterimage::components::enum_size); ++f) {
-				rasterimage::image im{expected, rasterimage::components(f), rasterimage::depth(d)};
+    for (auto d = 0; d != size_t(rasterimage::depth::enum_size); ++d) {
+      for (auto f = 0; f != size_t(rasterimage::components::enum_size); ++f) {
+        rasterimage::image im{expected, rasterimage::components(f),
+                              rasterimage::depth(d)};
 
-				tst::check_eq(im.dims(), expected, SL);
-			}
-		}
-	});
+        tst::check_eq(im.dims(), expected, SL);
+      }
+    }
+  });
 });
 } // namespace

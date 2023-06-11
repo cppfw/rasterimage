926b4a203086eb372073107030627f34b85c1963
Ivan Gagis <igagis@gmail.com>
2023-06-11 22:32:40 +0300

    fix conan and msys builds

--- conan/test_package/example.cpp
+++ conan/test_package/example.cpp
@@ -1,7 +1,7 @@
 #include <rasterimage/image.hpp>
 
 int main(int argc, const char** argv){
-	rasterimage::image<uint8_t, 4> im({100, 200});
+	rasterimage::image<uint8_t, 4> im(rasterimage::dimensioned::dimensions_type{100, 200});
 
 	std::cout << "image dims = " << im.dims() << std::endl;
 

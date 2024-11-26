0e68fd7165e61b2719df5c16898ffbd73512e4ea
Ivan Gagis <igagis@gmail.com>
2024-11-26 20:17:35 +0200

    vcpkgize

---
+++ vcpkg/test/main.cpp
@@ -0,0 +1,9 @@
+#include <rasterimage/image_variant.hpp>
+
+int main(int argc, const char** argv){
+    rasterimage::image_variant im(10, 20);
+
+    std::cout << "im.dims() = " << im.dims() << std::endl;
+
+    return 0;
+}

ed00e652daaa6a311d38389921cb47d13a08ec28
Ivan Gagis <igagis@gmail.com>
2024-11-26 20:30:21 +0200

    vcpkg: fix test

--- vcpkg/test/main.cpp
+++ vcpkg/test/main.cpp
@@ -1,7 +1,7 @@
 #include <rasterimage/image_variant.hpp>
 
 int main(int argc, const char** argv){
-    rasterimage::image_variant im(10, 20);
+    rasterimage::image_variant im({10, 20});
 
     std::cout << "im.dims() = " << im.dims() << std::endl;
 

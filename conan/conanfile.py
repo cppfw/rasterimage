25382683a772dda364637dee2cf2904c07e1288e
Ivan Gagis <igagis@gmail.com>
2024-12-03 14:27:10 +0200

    conan: macos

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -26,6 +26,8 @@ class RasterimageConan(ConanFile):
 	
 	def build_requirements(self):
 		self.requires("tst/[>=0.3.29]@cppfw/main", visible=False)
+		self.tool_requires("prorab/[>=2.0.27]@cppfw/main")
+		self.tool_requires("prorab-extra/[>=0.2.57]@cppfw/main")
 
 	def config_options(self):
 		if self.settings.os == "Windows":
@@ -51,8 +53,8 @@ class RasterimageConan(ConanFile):
 		git.run("submodule update --init --remote --depth 1")
 
 	def build(self):
-		self.run("make lint=off")
-		self.run("make lint=off test")
+		self.run("make $MAKE_INCLUDE_DIRS_ARG lint=off")
+		self.run("make $MAKE_INCLUDE_DIRS_ARG lint=off test")
 
 	def package(self):
 		src_dir = os.path.join(self.build_folder, "src")
